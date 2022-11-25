feb7d5380e8091721a71704c6c5e315db13c00c0
BoccoSantiago <italobocco@gmail.com>
2022-11-25 17:41:41 +0100

    a

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,9 @@
-import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import { Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 
-function Header() {
-  const { user, logout } = useAuth();
+import CartModal from "./CartModal";
 
+function Header({ cart }) {
   return (
     <div className=" d-flex justify-content-between bg-success bg-opacity-75 p-2">
       <Link
@@ -15,22 +13,10 @@ function Header() {
         SIAMI
       </Link>
 
-      {user ? (
-        <Stack direction="horizontal" gap={3}>
-          <Button onClick={logout} className="btn-light py-1">
-            Log out
-          </Button>
-        </Stack>
-      ) : (
-        <Stack direction="horizontal" gap={3}>
-          <Button as={Link} to="/login" className="btn-light py-1">
-            Login
-          </Button>
-          <Button as={Link} to="/register" className="btn-light py-1">
-            Register
-          </Button>
-        </Stack>
-      )}
+      <Stack direction="horizontal" gap={3}>
+        <CartModal cart={cart} />
+        <span>{cart.length}</span>
+      </Stack>
     </div>
   );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import ProtectedRoutes from "../components/protectedRoutes/ProtectedRoutes";
 import Dashboard from "../pages/Dashboard";
+import ProductList from "../pages/ProductList";
+import Checkout from "../pages/Checkout";
 
 export default function AppRoutes() {
+  const cartArray = [];
+  const [cart, setCart] = useState(cartArray);
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header cart={cart} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
+          <Route
+            path="/product-list"
+            element={<ProductList setCart={setCart} cart={cart} />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/checkout" element={<Checkout cart={cart} />} />
         </Routes>
       </BrowserRouter>
     </>
